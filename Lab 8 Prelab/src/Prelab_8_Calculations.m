d421ede7ee0f124d5dc7bd74b16d9c5966fd1c43
Matthew Mehrtens <mcmehrtens@icloud.com>
2023-03-31 19:18:21 -0500

    Finished Lab 8 Prelab

---
+++ Lab 8 Prelab/src/Prelab_8_Calculations.m
@@ -0,0 +1,63 @@
+% AER E 322 Prelab 8 Spring 2023
+% Matthew Mehrtens
+% Section 4 Group 2
+clear,clc;
+
+% Formula for Moment of Inertia of Specimen 1 & 2
+I = @(h, b, t) b * t^3 / 6 + b * t * h^2 / 2 ...
+    + t * (h - t)^3 / 12; % [inch^4]
+
+% Formula for r
+r = @(OD, t) (OD - t) / 2; % [inch]
+
+% Specimen 1 Parameters
+h_1 = 2.43; % [inch]
+b_1 = 1.456; % [inch]
+t_1 = 0.08; % [inch]
+I_1 = I(h_1, b_1, t_1); % [inch^4]
+
+% Specimen 2 Parameters
+h_2 = 0.84; % [inch]
+b_2 = 0.56; % [inch]
+t_2 = 0.055; % [inch]
+I_2 = I(h_2, b_2, t_2); % [inch^4]
+
+% Specimen 3 Parameters
+t_3 = 0.071; % [inch]
+OD_3 = 1.66; % [inch]
+theta_0_3 = 3.1 / 2; % [deg]
+r_3 = r(OD_3, t_3); % [inch]
+
+% Specimen 4 Parameters
+t_4 = 0.071; % [inch]
+OD_4 = 1.66; % [inch]
+theta_0_4 = 36.3 / 2; % [deg]
+r_4 = r(OD_4, t_4); % [inch]
+
+% Specimen 5 Parameters
+t_5 = 0.071; % [inch]
+OD_5 = 1.66; % [inch]
+theta_0_5 = 103.7 / 2; % [deg]
+r_5 = r(OD_5, t_5); % [inch]
+
+% Shear center calculations
+e_c = @(h, b, t, I) h^2 * b^2 * t / (4 * I); % [inch]
+e_circ = @(r, theta_0) 2 * r ...
+    * (cosd(theta_0) * (2 * pi - 2 * theta_0) + 2 * sind(theta_0)) ...
+    / (2 * pi - 2 * theta_0 + sind(2 * theta_0)); % [inch]
+
+e_1 = e_c(h_1, b_1, t_1, I_1); % [inch]
+e_2 = e_c(h_2, b_2, t_2, I_2); % [inch]
+e_3 = e_circ(r_3, theta_0_3); % [inch]
+e_4 = e_circ(r_4, theta_0_4); % [inch]
+e_5 = e_circ(r_5, theta_0_5); % [inch]
+
+% Print output
+fprintf("I_1 = %g [inch^4]\n" + ...
+    "I_2 = %g [inch^4]\n" + ...
+    "e_1 = %g [inch]\n" + ...
+    "e_2 = %g [inch]\n" + ...
+    "e_3 = %g [inch]\n" + ...
+    "e_4 = %g [inch]\n" + ...
+    "e_5 = %g [inch]\n", ...
+    [I_1, I_2, e_1, e_2, e_3, e_4, e_5]);
